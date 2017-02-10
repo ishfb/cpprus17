c3b318d7eaf4042da70d80a58cf3a4baf2a9c039
ishfb <shishkov.ilia@gmail.com>
2017-02-10 12:54:46 +0300

    Слайды для первоо прогона

--- code/coupling_2.cpp
+++ code/coupling_2.cpp
@@ -29,117 +29,137 @@ using namespace std;
 
 class Billing {
 public:
-	Billing(string usersDatabase, string tariffsDatabase) {
-		LoadUsers(usersDatabase);
-		LoadTariffs(tariffsDatabase);
-	}
+    Billing(string usersDatabase, string tariffsDatabase);
 
-	void ChargePhoneCall(int userId, int minutes) {
-		UserInfo& user = users_[userId];
-		const Tariff& t = tariffs_[user.GetTariff()];
-		user.ChargePhoneCall(minutes, t);
-	}
+    void ChargePhoneCall(int userId, int minutes);
+    void AddMoney(int userId, int rubles);
 
-	void AddMoney(int userId, int rubles) {
-		users_[userId].AddMoney(rubles);
-	}
-
-	void PrintStatsAsJson() {
-		cout << "[";
-		for (size_t i = 0; i < users_.size(); ++i) {
-			users_[i].PrintStatsAsJson();
-			if (i + 1 < users_.size()) {
-				cout << ",";
-			}
-		}
-		cout << "]";
-	}
+    void PrintStatsAsJson() {
+        cout << "[";
+        for (size_t i = 0; i < users_.size(); ++i) {
+            users_[i].PrintStatsAsJson();
+            if (i + 1 < users_.size()) {
+                cout << ",";
+            }
+        }
+        cout << "]";
+    }
 
 private:
-	void LoadUsers(string db);
-	void LoadTariffs(string db);
+    void LoadUsers(string db);
+    void LoadTariffs(string db);
 
-	class Event {
-	public:
-		static Event Payment(int rubles);
-		static Event PhoneCall(int minutes);
+    class Event {
+    public:
+        static Event Payment(int rubles);
+        static Event PhoneCall(int minutes);
 
-		bool IsPayment() const;
-		int GetMoney() const;
-		int GetDuration() const;
-	};
+        bool IsPayment() const;
+        int GetMoney() const;
+        int GetDuration() const;
+    };
 
-	struct Tariff {
-		int costPerMinute_;
-	};
+    struct Tariff {
+        int costPerMinute_;
+    };
 
-	class UserInfo {
-	public:
-		void ChargePhoneCall(int minutes, const Tariff& tariff) {
-			int charge = minutes * tariff.costPerMinute_;
-			money_ -= charge;
-			history_.push_back(Event::PhoneCall(minutes));
+    class UserInfo {
+    public:
+        void ChargePhoneCall(int minutes, const Tariff& tariff);
+        void AddMoney(int rubles);
+        int GetTariff() const;
 
-			if (money_ <= 0) {
-				SendSms("Your balance is " + to_string(money_));
-			}
+        void PrintStatsAsJson() {
+            cout << "{" << R"("callCount":)" << callCount_ << "," << R"("totalDuration":)"
+                 << totalDuration_ << "," << R"("moneySpent":)" << moneySpent_ << "}";
+        }
 
-			moneySpent_ += charge;
-			totalDuration_ += minutes;
-			++callCount_;
-		}
+    private:
+        void SendSms(string message);
 
-		void AddMoney(int rubles) {
-			money_ += rubles;
-			history_.push_back(Event::Payment(rubles));
-			SendSms("Got payment " + to_string(rubles));
-		}
+        int money_;
+        int tariff_;
+        int callCount_;
+        int totalDuration_;
+        int moneySpent_;
+        vector<Event> history_;
+    };
 
-		int GetTariff() const {
-			return tariff_;
-		}
-
-		void PrintStatsAsJson() {
-			cout << "{"
-				<< R"("callCount":)" << callCount_ << ","
-				<< R"("totalDuration":)" << totalDuration_ << ","
-				<< R"("moneySpent":)" << moneySpent_
-				<< "}";
-		}
-
-	private:
-		void SendSms(string message);
-
-		int money_;
-		int tariff_;
-		int callCount_;
-		int totalDuration_;
-		int moneySpent_;
-		vector<Event> history_;
-	};
-
-	vector<UserInfo> users_;
-	vector<Tariff> tariffs_;
+    vector<UserInfo> users_;
+    vector<Tariff> tariffs_;
 };
 
 int main() {
-	Billing b("users.sql", "tariffs.sql");
-	b.ChargePhoneCall(0, 2);
-	b.AddMoney(0, 10);
-	b.PrintStatsAsJson();
+    Billing b("users.sql", "tariffs.sql");
+    b.ChargePhoneCall(0, 2);
+    b.AddMoney(0, 10);
+    b.PrintStatsAsJson();
 
-	return 0;
+    return 0;
 }
 
 void Billing::UserInfo::SendSms(string message) {
 }
 
-Billing::Event Billing::Event::Payment(int rubles) { return {}; }
-Billing::Event Billing::Event::PhoneCall(int minutes) { return {}; }
-bool Billing::Event::IsPayment() const { return false; }
-int Billing::Event::GetMoney() const { return 5; }
-int Billing::Event::GetDuration() const { return 3; }
+void Billing::UserInfo::ChargePhoneCall(int minutes, const Billing::Tariff& tariff) {
+    int charge = minutes * tariff.costPerMinute_;
+    money_ -= charge;
+    history_.push_back(Event::PhoneCall(minutes));
 
-void Billing::LoadUsers(string db) { users_.resize(1); }
-void Billing::LoadTariffs(string db) { tariffs_.resize(1); }
+    if (money_ <= 0) {
+        SendSms("Your balance is " + to_string(money_));
+    }
+
+    moneySpent_ += charge;
+    totalDuration_ += minutes;
+    ++callCount_;
+}
+
+void Billing::UserInfo::AddMoney(int rubles) {
+    money_ += rubles;
+    history_.push_back(Event::Payment(rubles));
+    SendSms("Got payment " + to_string(rubles));
+}
+
+int Billing::UserInfo::GetTariff() const {
+    return tariff_;
+}
+
+Billing::Event Billing::Event::Payment(int rubles) {
+    return {};
+}
+Billing::Event Billing::Event::PhoneCall(int minutes) {
+    return {};
+}
+bool Billing::Event::IsPayment() const {
+    return false;
+}
+int Billing::Event::GetMoney() const {
+    return 5;
+}
+int Billing::Event::GetDuration() const {
+    return 3;
+}
+
+void Billing::LoadUsers(string db) {
+    users_.resize(1);
+}
+void Billing::LoadTariffs(string db) {
+    tariffs_.resize(1);
+}
+
+Billing::Billing(string usersDatabase, string tariffsDatabase) {
+    LoadUsers(usersDatabase);
+    LoadTariffs(tariffsDatabase);
+}
+
+void Billing::ChargePhoneCall(int userId, int minutes) {
+    UserInfo& user = users_[userId];
+    const Tariff& t = tariffs_[user.GetTariff()];
+    user.ChargePhoneCall(minutes, t);
+}
+
+void Billing::AddMoney(int userId, int rubles) {
+    users_[userId].AddMoney(rubles);
+}
 
